ca679194f18ba1bf761d0fc463dfa014c79eb4e8
udarasahan <udarasahanj@gmail.com>
2024-05-27 01:29:30 +0530

    hosting

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import './App.css'
-import { Home } from './Componants/Home.jsx/Home'
+import './App.css';
+import { Home } from './Componants/Home.jsx/Home';
+
 
 function App() {
- 
-
   return (
     <div>
       <Home />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/weather-dashboard/', 
   plugins: [react()],
-})
+});
